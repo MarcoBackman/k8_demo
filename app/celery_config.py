e3df2157f8fe3309cd72b41b80f149ef53444b50
백승준 <roni2006@hanmail.net>
2025-09-03 23:30:10 +0900

    Gave task specification to celery

--- app/celery_config.py
+++ app/celery_config.py
@@ -3,7 +3,9 @@ from celery import Celery
 celery_app = Celery(
     'tasks',
     broker='redis://redis-service:6379/0',
-    backend='redis://redis-service:6379/0'
+    backend='redis://redis-service:6379/0',
+    # This tells the worker to look for tasks inside 'celery_tasks/tasks.py'
+    include=['celery_tasks.tasks']
 )
 
 celery_app.conf.update(
